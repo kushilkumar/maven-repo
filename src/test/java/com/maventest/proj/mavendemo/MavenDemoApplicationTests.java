f7f6a06a958b8d1b8f42da9b3a9e50507c4d54e0
kushilkumar <kushilkumar000@gmail.com>
2024-03-14 10:23:55 +0530

    Initial commit

---
+++ src/test/java/com/maventest/proj/mavendemo/MavenDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maventest.proj.mavendemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
